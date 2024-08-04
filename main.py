ac6cddb817f5bc4180cde3394c2b243e6f90f0db
armin1864 <arminsuleimani@gmail.com>
2024-08-05 00:48:53 +0330

    login and signup completed

--- main.py
+++ main.py
@@ -2,9 +2,15 @@ import csv
 from usedMethods import clear_terminal
 from PyInquirer import prompt
 from rich import print
-from rich.layout import Layout
+from rich.console import Console
+from rich.table import Table
+from rich import box
+
+logged_user_phoneNumber = ""
+logged_user_fullName = ""
 
 
+# this method searches for username and password of users
 def user_login(username, password):
     with open("information.txt", "r") as infoFile:
         csv_reader = csv.reader(infoFile)
@@ -14,12 +20,129 @@ def user_login(username, password):
         return False
 
 
+# this method checks if new username exist in file or not
+def check_username_validity(username):
+    with open("information.txt", "r") as infoFile:
+        csv_reader = csv.reader(infoFile)
+        for line in csv_reader:
+            if line[2] == username:
+                return False
+        return True
+
+
+# this method checks if new phone number exist in file or not
+def check_number_validity(number):
+    with open("information.txt", "r") as infoFile:
+        csv_reader = csv.reader(infoFile)
+        for line in csv_reader:
+            if line[1] == number:
+                return False
+        return True
+
+
+def add_info_to_file(name, number, username, password):
+    with open("information.txt", "a") as infoFile:
+        new_info = "\n" + name + "," + number + "," + username + "," + password
+        infoFile.write(new_info)
+
+
+def find_user_info(username):
+    global logged_user_fullName, logged_user_phoneNumber
+    with open("information.txt", "r") as infoFile:
+        csv_reader = csv.reader(infoFile)
+        for line in csv_reader:
+            if line[2] == username:
+                logged_user_fullName = line[0]
+                logged_user_phoneNumber = line[1]
+
+
+# this is the method for first list options that user will see when run the code and can choose login / sign up or exit
+def first_menu():
+    questions = [
+        {
+            'type': 'list',
+            'name': 'option',
+            'message': 'Please choose an option:',
+            'choices': [
+                'Login',
+                'Sign up',
+                'Exit',
+            ]
+        }
+    ]
+    answers = prompt(questions)
+    choice = answers['option']
+
+    if choice == 'Login':
+        clear_terminal()
+        login_page()
+    elif choice == 'Sign up':
+        clear_terminal()
+        signup_page()
+    elif choice == 'Exit':
+        print('Exiting...')
+        return
+
+
+# this method will execute when admin logged in
+def admin_main_page():
+    pass
+
+
+# this method will execute when user logged in
+def user_main_page():
+    pass
+
+
+# gets user inputs for sign up and return them
+def signup_page_inputs():
+    console = Console()
+    console.rule("[bold italic royal_blue1]    Sign up    ")
+    print("[italic bold turquoise2] Please enter your information")
+    fields = [
+        {
+            'type': 'input',
+            'name': 'Name',
+            'message': 'Full Name : '
+        },
+        {
+            'type': 'input',
+            'name': 'PhoneNumber',
+            'message': 'Phone Number : '
+        },
+        {
+            'type': 'input',
+            'name': 'Username',
+            'message': 'Username : '
+        },
+        {
+            'type': 'password',
+            'name': 'Password',
+            'message': 'Password : '
+        }
+    ]
+    user_inputs = prompt(fields)
+    return user_inputs
+
+
+# checks the inputs validity and if was ok save them in file and return to log in
+def signup_page():
+    user_inputs = signup_page_inputs()
+    while not check_username_validity(user_inputs["Username"]):
+        print("[bold red] Username invalid. try again[/]")
+        user_inputs = signup_page_inputs()
+    while not check_number_validity(user_inputs["PhoneNumber"]):
+        print("[bold red] Phone Number invalid. try again[/]")
+        user_inputs = signup_page_inputs()
+    print("[bold green1]sign up complete... Please Login now[/]")
+    add_info_to_file(user_inputs['Name'], user_inputs['PhoneNumber'], user_inputs['Username'], user_inputs['Password'])
+    login_page()
+
+
+# this method will execute when user choose login from first menu
 def login_page():
-    clear_terminal()
-
-    layout = Layout()
-    print(layout)
-
+    console = Console()
+    console.rule("[bold italic royal_blue1]    Login    ")
     admin_username = "4[)m1n$P49[-"
     admin_password = "?4$$4[)JVI1^/"
     fields = [
@@ -36,17 +159,26 @@ def login_page():
     ]
     user_inputs = prompt(fields)
     if user_inputs["username"] == admin_username and user_inputs["password"] == admin_password:
-        print("welcome admin !!!")
+        admin_main_page()
     else:
         is_user = user_login(user_inputs["username"], user_inputs["password"])
         if is_user:
-            print("hello user !!!")
+            user_main_page()
+            find_user_info(user_inputs["username"])
         else:
+            # add options here
             print("not found")
 
 
 def main():
-    login_page()
+    clear_terminal()
+    # making the header for when user runs the code
+    first_page = Table(expand=True, style="cyan", box=box.DOUBLE_EDGE)
+    first_page.add_column("WELCOME TO RESERVATION SYSTEM", justify="center", style="magenta3")
+    first_page.add_row("Choose one of the options below")
+    console = Console()
+    console.print(first_page)
+    first_menu()
 
 
 if __name__ == "__main__":
--- usedMethods.py
+++ usedMethods.py
@@ -1,5 +1,3 @@
-from rich import print
-from PyInquirer import prompt
 import os
 
 
@@ -8,36 +6,3 @@ def clear_terminal():
         os.system("cls")
     else:
         os.system("clear")
-
-
-def menu():
-    questions = [
-        {
-            'type': 'list',
-            'name': 'option',
-            'message': 'Please choose an option:',
-            'choices': [
-                'available times for reservation',
-                'my reserved times',
-                'change information',
-                'Log out'
-            ]
-        }
-    ]
-
-    answers = prompt(questions)
-    choice = answers['option']
-
-    if choice == 'available times for reservation':
-        print('[reverse]You selected Option 1. [/]')
-        print("[blue bold italic] choose one of times")
-    elif choice == 'my reserved times':
-        print('You selected Option 2.')
-    elif choice == 'change information':
-        print('You selected Option 3.')
-    elif choice == 'Log out':
-        print('Exiting...')
-        return
-
-    # Call main again to keep the menu running
-    menu()
